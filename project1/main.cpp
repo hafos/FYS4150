e4970bcc35f2ced3e8a1defd1acee69b7040793c
edvhar <edvhar@outlook.com>
2021-09-13 01:35:33 +0200

    Added save to file and small index adjustments

--- project1/main.cpp
+++ project1/main.cpp
@@ -8,33 +8,39 @@ vector<double> c(n-1, -1); // superdiagonal
 vector<double> g(n, 0);
 
 vector<double> x_array(n, 0);
-double h = 1.0/n;
+double h = 1.0/(n-1);
 double h_squared = pow(h, 2.0); // to lessen FLOPs
 double x = 0;
 
-//double f2(double x);
-// void general_algorithm(vector<double> a, vector<double> b, vector<double> c, vector<double> g, vector<double> v);
 
 int main(){
 
     // output file
-    // std::string filename = "problem2_output.dat";
-    // std::ofstream ofile;
-    // ofile.open(filename);
+    std::string filename = "problem7_output.dat";
+    std::ofstream ofile;
+    ofile.open(filename);
     // spacing in outputfile
     int width = 12; int decimals = 4;
 
     // filling array
-    for (int i = 0; i <= n; i++){
+    for (int i = 0; i < n; i++){
     	x_array[i] = x;
         g[i] = f(x_array[i])*h_squared;
     	x += h;
     }
 
     vector<double> v = general_algorithm(n, a, b, c, g);
-    for (int i = 0; i < n; i++){
-        cout << v[i] << endl;
-    }
-    // ofile.close();
+    //for (int i = 0; i < n; i++){
+    //    cout << v[i] << endl;
+    //}
+
+    //  Writing solutions to file
+  	for (int i=0; i < n; i++){
+  		ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << x_array[i]
+  			  << std::setw(width) << std::setprecision(decimals) << std::scientific << v[i]
+  			  << std::endl;
+  	}
+
+    ofile.close();
     return 0;
 }
