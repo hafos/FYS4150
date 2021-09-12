5f1bfb73f0ea05adecd040ea9a9365f73714ef20
Oskar Hafstad <oskarhaf@student.matnat.uio.no>
2021-09-12 22:31:48 +0200

    Problem 7 is in a not entierly working state v array contains too large values

--- project1/main.hpp
+++ project1/main.hpp
@@ -1,15 +1,24 @@
 #ifdef main_hpp
 #define main_hpp
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
 double f(double x){
     return 100*exp(-10*x);
 }
 
-void general_algorithm(vector a, vector b, vector c, vector g, vector v){
+void general_algorithm(vector<double> a, vector<double> b, vector<double> c, vector<double> g, vector<double> v){
     // forward subst.
     double fraction; //  to lessen FLOPs
     g[0] =
-    for (int i = 0, i < n, i++){
+    for (int i = 0; i < n; i++){
         fraction = a[i]/b[i];
         b[i+1] = b[i+1] - fraction*c[i];
         g[i+1] = g[i+1] - fraction*g[i];
@@ -17,9 +26,10 @@ void general_algorithm(vector a, vector b, vector c, vector g, vector v){
     // backward subst.
     v[-1] = g[-1]/b[-1];
     // remembering indexing starting with 0 so n-2 starts from second last element
-    for (int i = n-2, 0 <= i, i--){
+    for (int i = n-2; 0 <= i; i--){
         v[i] = (g[i] - c[i]*v[i+1])/b[i];
     }
+    return;
 }
 
 
