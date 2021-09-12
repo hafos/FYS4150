a01d9692fadef5ce94743512f2b93da3db2ba2b1
edvhar <edvhar@outlook.com>
2021-09-13 01:08:17 +0200

    main.cpp and main.hpp are now functional
    Changed def of g, adjusted indexes, fixed header file

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,13 +1,7 @@
-// #include "main.hpp"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <iomanip>
-#include <vector>
-#include <cmath>
-using namespace std;
+#include "main.hpp"
 
-int n = 10; // grid points in square matrix
+
+int n = 100; // grid points in square matrix
 vector<double> a(n-1, -1); // subdiagonal
 vector<double> b(n, 2); // diagonal
 vector<double> c(n-1, -1); // superdiagonal
@@ -18,30 +12,8 @@ double h = 1.0/n;
 double h_squared = pow(h, 2.0); // to lessen FLOPs
 double x = 0;
 
-// double f(double x);
+//double f2(double x);
 // void general_algorithm(vector<double> a, vector<double> b, vector<double> c, vector<double> g, vector<double> v);
-
-double f(double x){
-    return 100*exp(-10*x);
-}
-
-vector<double> general_algorithm(vector<double> a, vector<double> b, vector<double> c, vector<double> g){
-    vector<double> v(n, 0);
-    // forward subst.
-    double fraction; //  to lessen FLOPs
-    for (int i = 0; i < n; i++){
-        fraction = a[i]/b[i];
-        b[i+1] = b[i+1] - fraction*c[i];
-        g[i+1] = g[i+1] - fraction*g[i];
-    }
-    // backward subst.
-    v[n-1] = g[n-1]/b[n-1];
-    // remembering indexing starting with 0 so n-2 starts from second last element
-    for (int i = n-2; 0 <= i; i--){
-        v[i] = (g[i] - c[i]*v[i+1])/b[i];
-    }
-    return v;
-}
 
 int main(){
 
@@ -55,11 +27,11 @@ int main(){
     // filling array
     for (int i = 0; i <= n; i++){
     	x_array[i] = x;
-        g[i] = f(x_array[i])/h_squared;
+        g[i] = f(x_array[i])*h_squared;
     	x += h;
     }
 
-    vector<double> v = general_algorithm(a, b, c, g);
+    vector<double> v = general_algorithm(n, a, b, c, g);
     for (int i = 0; i < n; i++){
         cout << v[i] << endl;
     }
--- project1/main.hpp
+++ project1/main.hpp
@@ -1,4 +1,4 @@
-#ifdef main_hpp
+#ifndef main_hpp
 #define main_hpp
 
 #include <iostream>
@@ -14,22 +14,22 @@ double f(double x){
     return 100*exp(-10*x);
 }
 
-void general_algorithm(vector<double> a, vector<double> b, vector<double> c, vector<double> g, vector<double> v){
+vector<double> general_algorithm(int n, vector<double> a, vector<double> b, vector<double> c, vector<double> g){
+    vector<double> v(n, 0);
     // forward subst.
     double fraction; //  to lessen FLOPs
-    g[0] =
-    for (int i = 0; i < n; i++){
-        fraction = a[i]/b[i];
-        b[i+1] = b[i+1] - fraction*c[i];
-        g[i+1] = g[i+1] - fraction*g[i];
+    for (int i = 1; i < n; i++){
+        fraction = a[i-1]/b[i-1];
+        b[i] = b[i] - c[i-1]*fraction;
+        g[i] = g[i] - g[i-1]*fraction;
     }
     // backward subst.
-    v[-1] = g[-1]/b[-1];
+    v[n-1] = g[n-1]/b[n-1];
     // remembering indexing starting with 0 so n-2 starts from second last element
     for (int i = n-2; 0 <= i; i--){
         v[i] = (g[i] - c[i]*v[i+1])/b[i];
     }
-    return;
+    return v;
 }
 
 
