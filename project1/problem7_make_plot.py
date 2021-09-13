57b90b6f39fa5657b8e02ba4909c2e08b02080c8
edvhar <edvhar@outlook.com>
2021-09-13 12:04:57 +0200

    Addded relative error plot and max rel err table

--- project1/problem7_make_plot.py
+++ project1/problem7_make_plot.py
@@ -49,7 +49,7 @@ for line in file_num:
 file_num.close()
 
 # Plot x and u(x):
-plt.figure()
+'''plt.figure()
 plt.plot(x, u, label='u(x)')
 for i in range(len(y)):
     n = len(y[i])
@@ -60,18 +60,20 @@ plt.grid()
 plt.title('Plot of the solution u(x) \nwith numerical aproximations v_i(n)')
 plt.legend()
 plt.savefig('problem7_output_plot.pdf')
-plt.show()
+plt.show()'''
 
 
 # Plot log of absolute error:
 plt.figure()
+u_i = [] # Store u for each run length
 for i in range(len(y)): # Loop over each run i
     n = len(y[i])
     k_closest = [] # Store index k of x that fit y_j here
     for j in range(len(y[i])): # Loop over all y_j
         arr = np.abs(np.array(x)-y[i][j]) # Find differences of all x to this y_j
         k_closest +=[np.argmin(arr)] # Store index k of x that fits best to y_j
-    plt.plot(y[i], np.log10(np.abs((np.array(u)[k_closest]-v[i]))),label=f'v$_i$: n=%g'%n)
+    u_i += [np.array(u)[k_closest]] # Add this run length u to list with all
+    plt.plot(y[i], np.log10(np.abs((u_i[i]-v[i]))),label=f'v$_i$: n=%g'%n)
 plt.xlabel(f'x$_i$')
 plt.ylabel(f'log$_1$$_0$($\Delta_i$)')
 plt.grid()
@@ -80,8 +82,30 @@ plt.legend()
 plt.savefig('problem8_log_abserr.pdf')
 plt.show()
 
+# Plot log of relative error:
+plt.figure()
+relerr = [] # Store relative errors for later
+for i in range(len(y)): # Loop over each run i
+    n = len(y[i])
+    relerr += [np.abs((u_i[i]-v[i])/u_i[i])]
+    plt.plot(y[i], np.log10(relerr[i]),label=f'v$_i$: n=%g'%n)
+plt.xlabel(f'x$_i$')
+plt.ylabel(f'log$_1$$_0$($\epsilon_i$)')
+plt.grid()
+plt.title('Logarithm of the relative error')
+plt.legend()
+plt.savefig('problem8_log_relerr.pdf')
+plt.show()
 
-
+""" Print table of max relative errors: """
+print('-----------------------------------------------------')
+print('Table of maximal relative errors')
+print('    n:     max(epsilon_i):')
+print('-----------------------------------------------------')
+for i in range(len(y)): # Loop over each run i
+    n = len(y[i])
+    print('%5g         %5.5e' %(n, np.nanmax(relerr[i])))
+print('-----------------------------------------------------')
 
 
 """
