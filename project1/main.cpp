fb84eccc5c1848ccd05f8c1cd79940b6a116ef33
edvhar <edvhar@outlook.com>
2021-09-13 02:13:01 +0200

    Restructuring and writing multiple runs to datafile
    The code now runs for n=10, n=100, n=1000 and saves them to file

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,20 +1,35 @@
 #include "main.hpp"
 
+vector<vector<double>> calculate_the_values(int n){
+  vector<double> a(n-1, -1); // subdiagonal
+  vector<double> b(n, 2); // diagonal
+  vector<double> c(n-1, -1); // superdiagonal
+  vector<double> g(n, 0);
 
-int n = 100; // grid points in square matrix
-vector<double> a(n-1, -1); // subdiagonal
-vector<double> b(n, 2); // diagonal
-vector<double> c(n-1, -1); // superdiagonal
-vector<double> g(n, 0);
+  vector<double> x_array(n, 0);
+  double h = 1.0/(n-1);
+  double h_squared = pow(h, 2.0); // to lessen FLOPs
+  double x = 0;
 
-vector<double> x_array(n, 0);
-double h = 1.0/(n-1);
-double h_squared = pow(h, 2.0); // to lessen FLOPs
-double x = 0;
+  // filling array
+  for (int i = 0; i < n; i++){
+    x_array[i] = x;
+      g[i] = f(x_array[i])*h_squared;
+    x += h;
+  }
+  vector<double> v = general_algorithm(n, a, b, c, g);
+  vector<vector<double>> xv(2,vector<double> (n, 0));
+  for (int i = 0; i < n; i++){
+    xv[0][i] = x_array[i];
+    xv[1][i] = v[i];
+  //    cout << v[i] << endl;
+  }
+  return xv;
+}
+
 
 
 int main(){
-
     // output file
     std::string filename = "problem7_output.dat";
     std::ofstream ofile;
@@ -22,24 +37,33 @@ int main(){
     // spacing in outputfile
     int width = 12; int decimals = 4;
 
-    // filling array
-    for (int i = 0; i < n; i++){
-    	x_array[i] = x;
-        g[i] = f(x_array[i])*h_squared;
-    	x += h;
-    }
-
-    vector<double> v = general_algorithm(n, a, b, c, g);
-    //for (int i = 0; i < n; i++){
-    //    cout << v[i] << endl;
-    //}
-
     //  Writing solutions to file
+    int n = 10; // grid points in square matrix
+    vector<vector<double>> xv_10 = calculate_the_values(n);
   	for (int i=0; i < n; i++){
-  		ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << x_array[i]
-  			  << std::setw(width) << std::setprecision(decimals) << std::scientific << v[i]
+  		ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << xv_10[0][i]
+  			  << std::setw(width) << std::setprecision(decimals) << std::scientific << xv_10[1][i]
+  			  << std::endl;
+    }
+    ofile << std::endl;
+
+    n = 100; // grid points in square matrix
+    vector<vector<double>> xv_100 = calculate_the_values(n);
+  	for (int i=0; i < n; i++){
+  		ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << xv_100[0][i]
+  			  << std::setw(width) << std::setprecision(decimals) << std::scientific << xv_100[1][i]
   			  << std::endl;
   	}
+    ofile << std::endl;
+
+    n = 1000; // grid points in square matrix
+    vector<vector<double>> xv_1000 = calculate_the_values(n);
+  	for (int i=0; i < n; i++){
+  		ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << xv_1000[0][i]
+  			  << std::setw(width) << std::setprecision(decimals) << std::scientific << xv_1000[1][i]
+  			  << std::endl;
+    }
+    ofile << std::endl;
 
     ofile.close();
     return 0;
