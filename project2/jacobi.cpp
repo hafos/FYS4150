2f7cd92fdb614e964c8bf772d97053dcc67df676
Oskar Hafstad <oskarhaf@student.matnat.uio.no>
2021-09-27 13:55:54 +0200

    fixed some spacings in jacobi.cpp

--- project2/jacobi.cpp
+++ project2/jacobi.cpp
@@ -12,46 +12,46 @@ void jacobi_rotate(arma::mat& B, arma::mat& R){
 
   // Get k, l, tau, t, c, s:
   double maxval = max_offdiag_symmetric(B, k, l); // sets new k and l
-  if(k==l){
+  if(k == l){
     std::cout << "Something went wrong: k = l" << endl;
   }
-  double tau = (B(l,l) - B(k,k))/(2*B(k,l));
+  double tau = (B(l, l) - B(k, k))/(2*B(k, l));
   double t;
-  if(tau>0){
+  if(tau > 0){
     t = - tau + sqrt(1 + pow(tau, 2));
   }
-  if(tau<=0){
+  if(tau <= 0){
     t = - tau - sqrt(1 + pow(tau, 2));
   }
-  double c = 1/sqrt(1+pow(t,2));
+  double c = 1/sqrt(1 + pow(t,2));
   double s = t*c;
 
   // Update B:
   //B = (S.t()*B)*S;
-  double B_kk = B(k,k)*pow(c, 2) - 2*B(k,l)*c*s + B(l,l)*pow(s,2);
-  B(l,l) = B(l,l)*pow(c, 2) + 2*B(k,l)*c*s + B(k,k)*pow(s,2);
-  B(k,k) = B_kk;
-  B(k,l) = 0;
-  B(l,k) = 0;
+  double B_kk = B(k, k)*pow(c, 2) - 2*B(k, l)*c*s + B(l, l)*pow(s, 2);
+  B(l, l) = B(l, l)*pow(c, 2) + 2*B(k, l)*c*s + B(k, k)*pow(s, 2);
+  B(k, k) = B_kk;
+  B(k, l) = 0;
+  B(l, k) = 0;
   double B_ik = 0;
-  for (int i=0; i<B.n_cols; i++){
-    if(i!=k){
-      if(i!=l){
-        B_ik = B(i,k)*c - B(i,l)*s;
-        B(k,i) = B_ik;
-        B(i,l) = B(i,l)*c + B(i,k)*s;
-        B(l,i) = B(i,l);
-        B(i,k) = B_ik;
+  for (int i=0; i < B.n_cols; i++){
+    if(i != k){
+      if(i != l){
+        B_ik = B(i, k)*c - B(i, l)*s;
+        B(k, i) = B_ik;
+        B(i, l) = B(i, l)*c + B(i, k)*s;
+        B(l, i) = B(i, l);
+        B(i, k) = B_ik;
       }
     }
   }
   // Update R:
   //R = R*S;
   double R_ik;
-  for (int i=0; i<B.n_cols; i++){
-    R_ik = R(i,k)*c - R(i,l)*s;
-    R(i,l) = R(i,l)*c + R(i,k)*s;
-    R(i,k) = R_ik;
+  for (int i=0; i < B.n_cols; i++){
+    R_ik = R(i, k)*c - R(i, l)*s;
+    R(i, l) = R(i, l)*c + R(i, k)*s;
+    R(i, k) = R_ik;
   }
 }
 
@@ -62,14 +62,14 @@ void jacobi_eigensolver(const arma::mat& A, double tolerance, arma::vec& eigenva
 
   // R = I
   mat R(A.n_cols, A.n_cols, fill::zeros);
-  for (int i=0; i<A.n_cols; i++){
-    R(i,i) = 1;
+  for (int i=0; i < A.n_cols; i++){
+    R(i, i) = 1;
   }
   // B = A
   mat B(A.n_cols, A.n_cols, fill::zeros);
-  for (int i=0; i<A.n_cols; i++){
-    for (int j=0; j<A.n_cols; j++){
-      B(i,j) = A(i,j);
+  for (int i=0; i < A.n_cols; i++){
+    for (int j=0; j < A.n_cols; j++){
+      B(i, j) = A(i, j);
     }
   }
 
@@ -93,10 +93,10 @@ void jacobi_eigensolver(const arma::mat& A, double tolerance, arma::vec& eigenva
       break;
     }
   }
-  for (int i=0; i<A.n_cols; i++){
+  for (int i=0; i < A.n_cols; i++){
     eigenvalues(i) = B(i,i);
-    for (int j=0; j<A.n_cols; j++){
-      eigenvectors(i,j) = R(i,j);
+    for (int j=0; j < A.n_cols; j++){
+      eigenvectors(i, j) = R(i, j);
     }
   }
 }
