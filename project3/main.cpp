ebd1a990d69dc5eeda41551f31f924c7c65084d0
edvhar <edvhar@outlook.com>
2021-10-24 13:00:36 +0200

    Modified main to separate multiple particle tests and single particle tests
    Executing main now runs the 5 single particle tests needed for problem 9

--- project3/main.cpp
+++ project3/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip> // Writing to file in nice format
+#include <string> // Turn to string
+#include <sstream> // String formatting
 #include <armadillo> // vectors and matrices
 #include <cmath>
 
@@ -8,7 +10,96 @@
 
 using namespace arma;
 
-int main()
+void single_particle_test(double dt)
+{
+  // Set parameters
+  double sim_time = 100; // [microseconds]
+  double B0_in = 9.65*1e1;
+  double V0_in = 9.65*1e8;
+  double d_in = 1e4; // [micrometers]
+
+  // Initialize the system
+  PenningTrap PT(B0_in, V0_in, d_in);
+  PenningTrap PT2(B0_in, V0_in, d_in);
+  // Add particles
+  double q_in = 1;
+  double m_in = 1;
+  vec r_in = { 10, 0, 5 };
+  vec v_in = { 0, -1, 0 };
+
+  Particle p_in = Particle(q_in, m_in, r_in, v_in);
+  PT.add_particle(p_in);
+  PT2.add_particle(p_in);
+
+  // Make test output files
+  std::ostringstream dt_string;
+  dt_string << std::setw(5) << std::setprecision(1) << std::scientific << dt;
+  std::string filename = "test_RK4-FE_dt" + dt_string.str() + ".dat";
+
+  std::ofstream ofile;
+  ofile.open(filename);
+
+  // spacing in outputfile
+  int width = 21; int decimals = 9;
+
+  // Write parameters to first line of file:
+  ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << B0_in
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << V0_in
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << d_in
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << q_in
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << m_in
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << v_in.at(1)
+    << std::endl;
+  ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << 0
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << r_in.at(0)
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << r_in.at(1)
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << r_in.at(2)
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << r_in.at(0)
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << r_in.at(1)
+    << std::setw(width) << std::setprecision(decimals) << std::scientific << r_in.at(2)
+    << std::endl;
+
+  // Test methods
+  double t = 0;
+  vec r(3);
+  vec r2(3);
+  bool stop_FE = 0;
+  while (t <= sim_time){
+    // Update:
+    PT.evolve_RK4(dt);
+    if (norm(PT2.particles_in_trap()[0].position) < d_in)
+    { // After this point it is pointless to continue updating, it just grows
+      PT2.evolve_forward_Euler(dt);
+      stop_FE = 1;
+    }
+    t += dt;
+    // Write to file:
+    r = PT.particles_in_trap()[0].position;
+    r2 = PT2.particles_in_trap()[0].position;
+    ofile << std::setw(width) << std::setprecision(decimals) << std::scientific << t
+        << std::setw(width) << std::setprecision(decimals) << std::scientific << r.at(0)
+        << std::setw(width) << std::setprecision(decimals) << std::scientific << r.at(1)
+        << std::setw(width) << std::setprecision(decimals) << std::scientific << r.at(2)
+        << std::setw(width) << std::setprecision(decimals) << std::scientific << r2.at(0)
+        << std::setw(width) << std::setprecision(decimals) << std::scientific << r2.at(1)
+        << std::setw(width) << std::setprecision(decimals) << std::scientific << r2.at(2)
+        << std::endl;
+    // Stop conditions:
+    if ((norm(PT.particles_in_trap()[0].position) > d_in) and (stop_FE == 1))
+    { // After this point it is pointless to continue updating, both are out of bounds
+      break;
+    }
+    if (dt <= 0)
+    {
+      std::cout << "What are you doing... dt should be positive!   dt: "<< dt << std::endl;
+      break;
+    }
+  }
+  // Close file
+  ofile.close();
+}
+
+void multiple_particle_test()
 {
   // Set parameters
   double sim_time = 1; // [microseconds]
@@ -81,4 +172,21 @@ int main()
   }
   // Close file
   ofile.close();
+}
+
+int main()
+{
+  // Test runs with single particle, using both FE and RK4
+  // They save position and time to file
+  // Use plot_tests.py to view the results
+  double dt = 0.01;
+  single_particle_test(dt);
+  dt = 0.005;
+  single_particle_test(dt);
+  dt = 0.001;
+  single_particle_test(dt);
+  dt = 0.0005;
+  single_particle_test(dt);
+  dt = 0.0001;
+  single_particle_test(dt);
 }
