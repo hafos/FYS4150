9f4e5ef65b2c820ede3c469658f8bbb6cd6bae7a
edvhar <edvhar@outlook.com>
2021-11-16 17:11:39 +0100

    Added analytical Cv and X solutions
    They fit with the results :)

--- project4/plot_tests_w_analytical.py
+++ project4/plot_tests_w_analytical.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def exp_epsilon(T, N):
     """
     T in units J/kB
@@ -18,16 +17,26 @@ def exp_magn(T, N):
 def exp_Cv(T, N):
     """
     T in units J/kB
+
+    Returns Cv in unit kB
     """
     beta = 1/T # [1/J]
-    return 0
+    E = exp_epsilon(T, N)*N
+    E2 = 64*np.cosh(8*beta)/(np.cosh(8*beta) + 3)
+    return (E2 - E**2)/(T**2*N) # [kB]
 
 def exp_X(T, N):
     """
     T in units J/kB
+
+    Returns X in unit 1/J
     """
     beta = 1/T # [1/J]
-    return 0
+    M = exp_magn(T, N)*N
+    M2 = (8*np.exp(8*beta) + 2)/((np.cosh(8*beta)+3))
+    return (M2 - M**2)/(T*N) # [1/J]
 
 print(exp_epsilon(1, 4))
 print(exp_magn(1, 4))
+print(exp_Cv(1, 4))
+print(exp_X(1, 4))
