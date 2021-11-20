d63b576e55adff4c51597a9de9a772c52f9264f2
edvhar <edvhar@outlook.com>
2021-11-21 00:40:11 +0100

    Separating main program for tests and for final runs

--- project4/README.md
+++ project4/README.md
@@ -1,3 +1,3 @@
-Compile using "g++ main.cpp src/*.cpp -I include/ -larmadillo -O2 -std=c++11 -o main.exe"
+Compile for tests using "g++ main_tests.cpp src/*.cpp -I include/ -larmadillo -O2 -std=c++11 -o main_tests.exe"
 
 The Lattice class header and source files are in /include and /src respectively.
---
+++ project4/main_parallelized.cpp
@@ -0,0 +1,17 @@
+#include <iomanip> // Writing to file
+#include <string> // Turn to string
+#include <sstream> // String formatting
+
+#include "Lattice.hpp"
+#include "timecheck.hpp"
+#include "markov_chain_mc.hpp"
+
+using namespace arma;
+
+int main()
+{
+  // Problem 7
+  // Check for time improvements
+
+
+}
