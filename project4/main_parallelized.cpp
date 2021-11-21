cdc03de0c26af7f4d6d8005b5ae36cb50eb782e4
edvhar <edvhar@outlook.com>
2021-11-21 03:24:09 +0100

    Fixed compilation error

--- project4/main_parallelized.cpp
+++ project4/main_parallelized.cpp
@@ -17,14 +17,14 @@ int main()
   // Problem 8
   // Run final estimations with parallelized code
   ivec L = {40};//, 60, 80 100};
-  vec T = linspace(2.1, 2.4, 2);
-  bool Ordered = 0; // Less random fluctuations in computation time?
+  vec T = linspace(2.1, 2.4, 2); // Run with 10 first
+  bool Ordered = 0; // Start with random spins
   int n_burnin = 1000;
-  int n_cycles = 1000;
+  int n_cycles = 2000;
 
   for (int j=0; j<L.n_elem; j++)
   {
-    int N = L(i)*L(i);
+    int N = L(j)*L(j);
 
     vec energy(T.n_elem);
     vec magnetization(T.n_elem);
@@ -36,15 +36,16 @@ int main()
     #pragma omp parallel for
     for (int i=0; i<T.n_elem; i++)
     {
-      expectation_values(L(i), T(i), Ordered, n_cycles, n_burnin, energy(i),
+      expectation_values(L(j), T(i), Ordered, n_cycles, n_burnin, energy(i),
                           magnetization(i), heat_capacity(i), susceptibility(i));
     }
     // End of parallelization
 
     // Open outputfile
+    std::ofstream ofile;
     std::ostringstream lstring;
-    lstring << L(i);
-    std::string filename = "results_L" + lstring.str() + ".dat"
+    lstring << L(j);
+    std::string filename = "results_L" + lstring.str() + ".dat";
     ofile.open(filename);
     ofile << "n_cycles: "
     << std::setw(width) << std::setprecision(decimals) << std::scientific << n_cycles << std::endl;
