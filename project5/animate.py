0a62ee2f6669b8381215cd8e24250d56976475c0
katherle <katherine.lee@astro.uio.no>
2021-12-08 22:44:03 +0100

    Fixed an incorrect colorbar label

--- project5/animate.py
+++ project5/animate.py
@@ -29,7 +29,7 @@ def animate_result(x_points, y_points, z_data_list, t_points):
 
     # Add a colourbar
     cbar = fig.colorbar(img, ax=ax)
-    cbar.set_label("u(x,y,t)", fontsize=fontsize)
+    cbar.set_label(r"$p_{ij}^n$", fontsize=fontsize)
     cbar.ax.tick_params(labelsize=fontsize)
 
     # Add a text element showing the time
