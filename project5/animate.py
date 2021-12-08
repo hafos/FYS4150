38f865d68d09c78a0272c0bfd4d76bc5d2909d7f
edvhar <edvhar@outlook.com>
2021-12-08 17:38:07 +0100

    added animation code
    a function in animate.py to animate in general
    +
    a line at end of plot program that animates 7b

---
+++ project5/animate.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+def animate_result(x_points, y_points, z_data_list, t_points):
+    dt = t_points[1]-t_points[0]
+    # Some settings
+    fontsize = 12
+    t_min = t_points[0]
+    x_min, x_max = x_points[0], x_points[-1]
+    y_min, y_max = y_points[0], y_points[-1]
+
+    # Create figure
+    fig = plt.figure()
+    ax = plt.gca()
+
+    # Create a colour scale normalization according to the max z value in the first frame
+    norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=np.max(z_data_list[0]))
+
+    # Plot the first frame
+    img = ax.imshow(z_data_list[0], extent=[x_min,x_max,y_min,y_max], cmap=plt.get_cmap("viridis"), norm=norm)
+
+    # Axis labels
+    plt.xlabel("x", fontsize=fontsize)
+    plt.ylabel("y", fontsize=fontsize)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+
+    # Add a colourbar
+    cbar = fig.colorbar(img, ax=ax)
+    cbar.set_label("z(x,y,t)", fontsize=fontsize)
+    cbar.ax.tick_params(labelsize=fontsize)
+
+    # Add a text element showing the time
+    time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white",
+                        horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
+
+    # Function that takes care of updating the z data and other things for each frame
+    def animation(i):
+        # Normalize the colour scale to the current frame?
+        norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=np.max(z_data_list[i]))
+        img.set_norm(norm)
+
+        # Update z data
+        img.set_data(z_data_list[i])
+
+        # Update the time label
+        current_time = t_min + i * dt
+        time_txt.set_text("t = {:.3e}".format(current_time))
+
+        return img
+
+    # Use matplotlib.animation.FuncAnimation to put it all together
+    anim = FuncAnimation(fig, animation, interval=1, frames=np.arange(0, len(z_data_list), 2), repeat=False, blit=0)
+
+    # Run the animation!
+    #plt.show()
+
+    # # Save the animation
+    anim.save('./animation.mp4', writer="ffmpeg", bitrate=-1, fps=30)
--- project5/plot_simulation.py
+++ project5/plot_simulation.py
@@ -7,16 +7,20 @@ from matplotlib.animation import FuncAnimation
 from matplotlib import cm
 from cycler import cycler
 
+from animate import animate_result # Animation function example
+
 prob_7a = pa.cube()
 prob_7a.load("probability_prob7a.bin")
 prob_7a = np.array(prob_7a)
-#print(prob_7a.shape)
+print(prob_7a.shape)
 
 total_prob_7a = np.zeros(prob_7a.shape[0])
 for i in range(prob_7a.shape[0]):
     total_prob_7a[i] = np.sum(prob_7a[i])
 
 t = np.linspace(0, 0.008, prob_7a.shape[0])
+x = np.linspace(0, 1, prob_7a.shape[1])
+y = np.linspace(0, 1, prob_7a.shape[2])
 #print(total_prob_7a)
 
 plt.figure()
@@ -32,10 +36,10 @@ fig, ax = plt.subplots(2, 2)
 im0 = ax[0, 0].imshow(prob_7a[0].transpose(), extent = extent,
                         vmin = 0, vmax = np.amax(prob_7a[0]), cmap = 'plasma')
 ax[0, 0].set_title('Initial conditions')
-im1 = ax[0, 1].imshow(prob_7a[int(prob_7a.shape[0]/4)].transpose(), extent = extent,
+im1 = ax[0, 1].imshow(prob_7a[int(prob_7a.shape[0]/3)].transpose(), extent = extent,
                         vmin = 0, vmax = np.amax(prob_7a[0]), cmap = 'plasma')
 ax[0, 1].set_title('Snapshot 1')
-im2 = ax[1, 0].imshow(prob_7a[int(prob_7a.shape[0]*3/4)].transpose(), extent = extent,
+im2 = ax[1, 0].imshow(prob_7a[int(prob_7a.shape[0]*2/3)].transpose(), extent = extent,
                         vmin = 0, vmax = np.amax(prob_7a[0]), cmap = 'plasma')
 ax[1, 0].set_title('Snapshot 2')
 im3 = ax[1, 1].imshow(prob_7a[prob_7a.shape[0]-1].transpose(), extent = extent,
@@ -70,10 +74,10 @@ fig, ax = plt.subplots(2, 2)
 im0 = ax[0, 0].imshow(prob_7b[0].transpose(), extent = extent,
                         vmin = 0, vmax = np.amax(prob_7b[0]), cmap = 'plasma')
 ax[0, 0].set_title('Initial conditions')
-im1 = ax[0, 1].imshow(prob_7b[int(prob_7b.shape[0]/4)].transpose(), extent = extent,
+im1 = ax[0, 1].imshow(prob_7b[int(prob_7b.shape[0]/3)].transpose(), extent = extent,
                         vmin = 0, vmax = np.amax(prob_7b[0]), cmap = 'plasma')
 ax[0, 1].set_title('Snapshot 1')
-im2 = ax[1, 0].imshow(prob_7b[int(prob_7b.shape[0]*3/4)].transpose(), extent = extent,
+im2 = ax[1, 0].imshow(prob_7b[int(prob_7b.shape[0]*2/3)].transpose(), extent = extent,
                         vmin = 0, vmax = np.amax(prob_7b[0]), cmap = 'plasma')
 ax[1, 0].set_title('Snapshot 2')
 im3 = ax[1, 1].imshow(prob_7b[prob_7b.shape[0]-1].transpose(), extent = extent,
@@ -81,3 +85,5 @@ im3 = ax[1, 1].imshow(prob_7b[prob_7b.shape[0]-1].transpose(), extent = extent,
 ax[1, 1].set_title('Final outcome')
 [fig.colorbar(imi, ax=axi, shrink=0.8, label = 'PDF') for imi, axi in zip([im0, im1, im2, im3], ax)]
 plt.show()
+
+animate_result(x, y, prob_7b, t)
