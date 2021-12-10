51c9f33335097fac043b7c614b409396ebe39812
edvhar <edvhar@outlook.com>
2021-12-10 22:05:09 +0100

    added option to save either wavefunction or probability
    In most cases saving the probability (one file) is enough.
    In other cases we need to save the entire wavefunction (two files).

--- project5/main_simulation.cpp
+++ project5/main_simulation.cpp
@@ -13,6 +13,7 @@ int main(int argc, char* argv[])
   // Get configuration from config file or command-line:
   double h_in, dt_in, Tmax_in, xc_in, sx_in, px_in, yc_in, sy_in, py_in, v0_in;
   int n_slits;
+  bool save_prob; // If we want to save the probability only (1 file instead of 2)
   std::string filename;
   if (argc == 2)
   {
@@ -40,7 +41,7 @@ int main(int argc, char* argv[])
           }
           // Save the contents of the line :
           std::stringstream mysstream(line);
-          mysstream >> h_in >> dt_in >> Tmax_in >> xc_in >> sx_in >> px_in >> yc_in >> sy_in >> py_in >> v0_in >> n_slits >> filename;
+          mysstream >> h_in >> dt_in >> Tmax_in >> xc_in >> sx_in >> px_in >> yc_in >> sy_in >> py_in >> v0_in >> n_slits >> save_prob >> filename;
           linecount += 1;
         }
       }
@@ -56,13 +57,13 @@ int main(int argc, char* argv[])
   {
     // Check number of command-line arguments
 
-    if (argc != 13)  // Expect 12 command-line arguments
+    if (argc != 14)  // Expect 13 command-line arguments
     {
       // Get the name of the executable file
       std::string executable_name = argv[0];
 
       std::cerr << "Error: Wrong number of input arguments." << std::endl;
-      std::cerr << "Usage: " << executable_name << " <double h> <double dt> <double T> <double xc> <double sx> <double px> <double yc> <double sy> <double py> <double v0> <int n_slits> <string filename>" << std::endl;
+      std::cerr << "Usage: " << executable_name << " <double h> <double dt> <double T> <double xc> <double sx> <double px> <double yc> <double sy> <double py> <double v0> <int n_slits> <bool save_prob> <string filename>" << std::endl;
       std::cerr << "Or     " << executable_name << " <string config_filename>" << std::endl;
 
       // Exit program with non-zero return code to indicate a problem
@@ -81,17 +82,23 @@ int main(int argc, char* argv[])
     py_in = atof(argv[9]); // initial momentum of wave packet (y)
     v0_in = atof(argv[10]); // potential barrier height
     n_slits = atoi(argv[11]); // number of slits, must be positive or 0
-    filename = argv[12]; // filename to save data to
+    save_prob = atoi(argv[12]); // If 1 saves only probability. If 0 saves real+imag.
+    filename = argv[13]; // filename to save data to (base)
   }
 
-  std::string filename_real = filename;
-  filename_real.insert(filename.length()-4, "_real");
-  std::string filename_imag = filename;
-  filename_imag.insert(filename.length()-4, "_imag");
+  std::string filename_real;
+  std::string filename_imag;
+  if (save_prob == 0)
+  {
+    filename_real = filename + "_real.bin";
+    filename_imag = filename + "_imag.bin";
+  }
+
+  filename += ".bin";
 
   std::cout << endl;
   std::cout << "Configuration :" << endl;
-  std::cout << "---------------------------------------" << endl;
+  std::cout << "----------------------------------" << endl;
   std::cout << "       h : " << h_in << endl;
   std::cout << "      dt : " << dt_in << endl;
   std::cout << "       T : " << Tmax_in << endl;
@@ -103,9 +110,17 @@ int main(int argc, char* argv[])
   std::cout << "      py : " << py_in << endl;
   std::cout << "      v0 : " << v0_in << endl;
   std::cout << " n_slits : " << n_slits << endl;
-  std::cout << "filename : " << filename << endl;
-  std::cout << "filename_real : " << filename_real << endl;
-  std::cout << "filename_imag : " << filename_imag << endl << endl;
+  std::cout << "---------------------------------" << endl;
+  std::cout << "saving probability only (on/off): " << save_prob << endl;
+  if (save_prob)
+  {
+    std::cout << "prob only filename : " << filename << endl;
+  }
+  else
+  {
+    std::cout << "filename_real : " << filename_real << endl;
+    std::cout << "filename_imag : " << filename_imag << endl << endl;
+  }
 
   int M = 1/h_in + 1;
   int N = M-2;
@@ -121,36 +136,43 @@ int main(int argc, char* argv[])
   sp_cx_mat B(N2, N2);
   initialize_matrices(M, h_in, dt_in, V, A, B); // sets up Crank-Nicolson
 
-  // loop over time
-  // store initial condition as first slice of probability density cube
-  cube P_n(M, M, 1, fill::zeros);
-  cube Preal(M, M, 1, fill::zeros);
-  cube Pconj(M, M, 1, fill::zeros);
-  P_n.slice(0) = real(U%conj(U));
-  Preal.slice(0) = real(U);
-  Pconj.slice(0) = imag(U);
+  // 1. store initial condition as first slice of cube
+  // 2. Loop over time and add slices to cube
+  // 3. Save cube as binary file after ended loop
 
   double t = 0;
-  mat p(M, M);
-  while (t < Tmax_in)
+  if (save_prob)
   {
-    u_vec = compute_next_step(u_vec, A, B);
-    U = vec2mat(M, u_vec);
-    p = real(U%conj(U));
-    P_n = join_slices(P_n, p);
-    Preal = join_slices(Preal, real(U));
-    Pconj = join_slices(Pconj, imag(U));
-    t = t+dt_in;
+    cube P_n(M, M, 1, fill::zeros);
+    P_n.slice(0) = real(U%conj(U));
+    while (t < Tmax_in)
+    {
+      u_vec = compute_next_step(u_vec, A, B);
+      U = vec2mat(M, u_vec);
+      mat p = real(U%conj(U));
+      P_n = join_slices(P_n, p);
+      t = t+dt_in;
+    }
+    // Save as binary data file :
+    P_n.save(filename);
   }
-
-  //std::cout << P_n.tail_slices(1) << endl;
-
-  //P_n.each_slice( [](mat& X){ std::cout << accu(X) << endl; } );
-
-  // save as binary data file
-  P_n.save(filename);
-  Preal.save(filename_real);
-  Pconj.save(filename_imag);
-
+  else
+  {
+    cube Preal(M, M, 1, fill::zeros);
+    cube Pconj(M, M, 1, fill::zeros);
+    Preal.slice(0) = real(U);
+    Pconj.slice(0) = imag(U);
+    while (t < Tmax_in)
+    {
+      u_vec = compute_next_step(u_vec, A, B);
+      U = vec2mat(M, u_vec);
+      Preal = join_slices(Preal, real(U));
+      Pconj = join_slices(Pconj, imag(U));
+      t = t+dt_in;
+    }
+    // Save as binary data file :
+    Preal.save(filename_real);
+    Pconj.save(filename_imag);
+  }
 
 }
