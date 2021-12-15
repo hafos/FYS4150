f0229562b0045bfec436b06a75ca0dbddc57bfdb
Oskar Hafstad <oskarhaf@student.matnat.uio.no>
2021-12-15 22:40:44 +0100

    spacing and finished location of detector plots

--- project5/plot_simulation.py
+++ project5/plot_simulation.py
@@ -51,10 +51,6 @@ fig.tight_layout()
 plt.savefig('probability_deviation_checks.pdf')
 
 
-
-
-
-
 """ Plot colormaps of Re(u), Im(u) and p for double-slit simulation """
 # Open imaginary and real results and combine to get the probability:
 prob_8_imag = pa.cube()
@@ -68,8 +64,7 @@ prob_8_real = np.array(prob_8_real)
 prob_8 = prob_8_real**2 + prob_8_imag**2 # p = u^*u
 
 
-
-##### Plot all in one 3x3 figure: (Alternative 1)
+##### Plot all in one 3x3 figure:
 ticks = np.linspace(0.1, 0.9, 5) # Specifies placement of xticks/yticks for closely spaced figures.
 extent = [0, 1, 0, 1] # Extent of the plot
 fig, ax = plt.subplots(3, 3, figsize = (15/1.3,13.5/1.3), sharey=True, sharex=True, constrained_layout=True)
@@ -108,9 +103,6 @@ fig.set_constrained_layout_pads(w_pad=0.1 / 144, h_pad=0.1 / 144, hspace=0.01, w
 plt.savefig('nine_panels.pdf')
 
 
-
-
-
 """ Plotting detection probabilities along screen for single-, double- and triple-slit cases: """
 x08 = np.argmin(np.abs(x-0.8)) # argument position where x = 0.8
 
@@ -138,12 +130,35 @@ prob_9_3_slits = np.array(prob_9_3_slits)
 slice = prob_9_3_slits[-1, x08] # Extract slice at screen position
 plt.plot(y, slice/np.sum(slice), label='Triple-slit', color='tab:olive') # Plot normalized probability
 
-
 plt.legend()
 plt.title('Detection probability along detector screen')
 plt.tight_layout()
 plt.savefig('detection_screen.pdf')
 
+
+""" Marking where the detections are made """
+fig, ax = plt.subplots(1, 3, figsize = (15, 5), sharey=True)
+im0 = ax[0].imshow(prob_9_1_slit[prob_9_1_slit.shape[0]-1].transpose(), extent = extent,
+                        vmin = 0, vmax = np.amax(prob_9_1_slit[0]), cmap = 'plasma')
+ax[0].set_title('Single-slit')
+ax[0].vlines(x=0.8, ymin=0, ymax=1, color=("#1f77b4"), linestyle='--')
+ax[0].set_ylabel('y')
+im1 = ax[1].imshow(prob_8[prob_8.shape[0]-1].transpose(), extent = extent,
+                        vmin = 0, vmax = np.amax(prob_8[0]), cmap = 'plasma')
+ax[1].set_title('Double-slit')
+ax[1].vlines(x=0.8, ymin=0, ymax=1, color='red', linestyle='--')
+im2 = ax[2].imshow(prob_9_3_slits[prob_9_3_slits.shape[0]-1].transpose(), extent = extent,
+                        vmin = 0, vmax = np.amax(prob_9_3_slits[0]), cmap = 'plasma')
+ax[2].set_title('Triple-slit')
+ax[2].vlines(x=0.8, ymin=0, ymax=1, color='olive', linestyle='--')
+cax = fig.add_axes([ax[2].get_position().x1+0.01,ax[2].get_position().y0,0.02,ax[2].get_position().height])
+# fig.set_constrained_layout_pads(w_pad=0.1 / 144, h_pad=0.1 / 144, hspace=0.01, wspace=0.01)
+fig.colorbar(im2, ax=ax[2], cax=cax).set_label(r'$p = u^{*}u$',labelpad=3, y=0.5)
+[ax[i].set_xlabel('x') for i in range(3)]
+ax[0].text(s='$t$ = 0.002', y=0.9, x=0.35, color='w', fontsize=14)
+ax[1].text(s='$t$ = 0.002', y=0.9, x=0.35, color='w', fontsize=14)
+ax[2].text(s='$t$ = 0.002', y=0.9, x=0.35, color='w', fontsize=14)
+plt.savefig('detection_location.pdf')
 
 plt.close('all') # Closes current figures so that they are not shown in animation code
 
